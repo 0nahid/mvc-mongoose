1d9eff9fcf00d316a81df5a29068cbeae062b1cb
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-09-18 01:38:59 +0600

    vercel deploy

---
+++ dist/utils/dbConnect.js
@@ -0,0 +1,27 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const colors_1 = __importDefault(require("colors"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const MONGO_URI = process.env.MONGO_URI || "";
+const dbConnect = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield mongoose_1.default.connect(MONGO_URI);
+        console.log(colors_1.default.cyan.italic(`Database connection is successful 🛢`));
+    }
+    catch (error) {
+        console.log(colors_1.default.red.italic(`Database connection failed 🛢`));
+    }
+});
+exports.default = dbConnect;
